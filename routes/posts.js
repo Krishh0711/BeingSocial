3b64a424036105296cd5d6aed46c5cad95403e80
Krishh258 <krishnanshu33@gmail.com>
2020-06-12 04:50:28 +0530

    added check on post

--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const passport = require('passport');
+
 const postController = require('../controllers/post_controller');
 
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 
 
 
